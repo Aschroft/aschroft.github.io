82a7b510e8ec6a91b9100478025814d16a929e40
Aschroft <chrisayrton@icloud.com>
2019-06-18 15:42:30 +0700

    Add files via upload

---
+++ test.js
@@ -0,0 +1,11 @@
+var iceCream = 'chocolate';
+if (iceCream === 'chocolate') {
+    alert('Yay, I love chocolate ice cream!');
+} else {
+    alert('Awwww, but chocolate is my favorite...');
+}
+
+function multiply(num1, num2) {
+    var result = num1 * num2;
+    return result;
+}
